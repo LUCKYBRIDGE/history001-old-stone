555b414484be0bc95a854651697a49873ce76bc8
LUCKYBRIDGE <lucky20220528@gmail.com>
2026-08-26 20:17:24 +0900

    Load Stage 07 human QA visual fixes

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
 import './styles/tokens.css';
 import './styles/global.css';
+import './styles/r2HumanQaVisualFix.css';
 
 const rootElement = document.getElementById('root');
 
